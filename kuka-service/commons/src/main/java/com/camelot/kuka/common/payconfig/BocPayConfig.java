f3c239efd9cfb149622b914487a0646c7d51393c
cuichunsong <136122030@qq.com>
2020-03-23 21:35:00 +0800

    修改支付配置

--- kuka-service/commons/src/main/java/com/camelot/kuka/common/payconfig/BocPayConfig.java
+++ kuka-service/commons/src/main/java/com/camelot/kuka/common/payconfig/BocPayConfig.java
@@ -6,7 +6,7 @@ public class BocPayConfig {
     public static String signKeyFile = "/data/payfile/config/payment/test.pfx";    // 签名证书
     public static String signkeyPassword = "11111111"; // 签名证书密码
     public static String verifyCerFile = "/data/payfile/config/payment/bocnetcaSIT.cer"; // 验签的中行根证书
-    public static String notify_url = "http://161.189.2.249:1111/bocpay/notice";//异步通知
-    public static String return_url = "http://161.189.2.249/";//跳转页面
+    public static String notify_url = "http://52.83.60.249:1111/bocpay/notice";//异步通知
+    public static String return_url = "http://52.83.60.249/";//跳转页面
     public static String merchantNo = "";//商户编码
 }
