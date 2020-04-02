c5ddf1c8f93ae8c2b7bd8b751e7e8e0a5e030289
cuichunsong <136122030@qq.com>
2020-04-02 13:44:34 +0800

    修改中行异步通知地址

--- kuka-service/commons/src/main/java/com/camelot/kuka/common/payconfig/BocPayConfig.java
+++ kuka-service/commons/src/main/java/com/camelot/kuka/common/payconfig/BocPayConfig.java
@@ -8,7 +8,7 @@ public class BocPayConfig {
     public static String signkeyPassword = "123456"; // 签名证书密码
     public static String verifyCerFile = "/data/payfile/config/payments/bocnetcaSIT.cer"; // 验签的中行根证书
     //public static String verifyCerFile = "/Users/cuichunsong/ccs/d/jar/payments/bocnetcaSIT.cer";
-    public static String notify_url = "http://52.83.60.249:1111/bocpay/notice";//异步通知
+    public static String notify_url = "http://52.83.60.249/api/api-b/bocpay/notice";//异步通知
     public static String return_url = "http://52.83.60.249/";//跳转页面
     public static String merchantNo = "6876";//商户编码
 }
